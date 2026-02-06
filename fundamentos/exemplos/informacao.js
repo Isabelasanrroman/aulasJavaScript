d3febbdbb0c63daa21f22b5a56c371e7e9ea8811
Isabela sanrroman <isabela.sanrroman@portalsesisp.org.br>
2026-02-06 09:22:10 -0300

    arquivo informacao

---
+++ fundamentos/exemplos/informacao.js
@@ -0,0 +1,12 @@
+const curso = "Técnico em Desenvolvimento de Sistemas"
+const ano = 2025
+const notaMinimaPraSerAprovado = 50
+const notaParaSerReprovado = 49.9
+const cursoTecnico = true
+
+console.log("Minhas informações academicas")
+console.log("Curso: Desenvolvimento de Sistemas")
+console.log(`Ano: ${ano}`)
+console.log(`Nota para aprovação: ${notaMinimaPraSerAprovado}`)
+console.log(`Nota mínima para ser reprovado: ${notaParaSerReprovado}`)
+console.log(`É o curso técnico: ${cursoTecnico}`)
